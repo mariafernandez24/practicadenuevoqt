a7cd143bdbb80cc195993f6b82b5409b05962b74
maria <mariafernandezj24@gmail.com>
2026-06-09 10:28:21 +0200

    inicio

---
+++ config.h
@@ -0,0 +1,4 @@
+// Valores de configuración que se han de tener en cuenta (posiblemente en ambos proyectos)
+#define MAX_FUEL 100  // Combustible máximo
+#define MAX_VELOCIDAD 240  // Máxima velocidad (Km/h)
+#define ALTURA_INICIAL 3000
